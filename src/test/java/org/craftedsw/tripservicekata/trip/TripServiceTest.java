7576f64f78fe2503c369541280c4b23ca1b44924
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-09-04 18:58:52 +0100

    Refactor: clearer tests to improve readability

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -20,39 +21,53 @@ public class TripServiceTest {
     private User user;
     private User loggedUser;
     private TripsUserRepository tripsUserRepository;
+    private LoggedUserService loggedUserService;
+    private Trip trip;
 
     @Before
     public void setUp() throws Exception {
         loggedUser = new User();
         user = new User();
-        LoggedUserService loggedUserService = mock(LoggedUserService.class);
-        when(loggedUserService.getLoggedUser()).thenReturn(loggedUser);
+        trip = new Trip();
+        loggedUserService = mock(LoggedUserService.class);
         tripsUserRepository = mock(TripsUserRepository.class);
         service = new TripService(loggedUserService, tripsUserRepository);
     }
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_raise_an_exception_when_user_is_not_logged() throws Exception {
-        LoggedUserService notLoggedUserService = mock(LoggedUserService.class);
-        when(notLoggedUserService.getLoggedUser()).thenThrow(new UserNotLoggedInException());
-        TripService tripService = new TripService(notLoggedUserService, tripsUserRepository);
-        tripService.getTripsByUser(user);
+        GivenANotLoggedUser();
+        service.getTripsByUser(user);
     }
 
     @Test
     public void should_return_empty_trip_list_when_loggedUser_is_not_a_friend_of_user() {
+        GivenALoggedUser(loggedUser);
         when(tripsUserRepository.getTripsOf(user)).thenReturn(new ArrayList<>());
         assertThat(service.getTripsByUser(user), is(Arrays.asList()));
     }
 
-
     @Test
     public void should_return_list_with_the_trips_of_user_when_is_friend_of_loggedUser() {
-        Trip trip = new Trip();
-        user.addFriend(loggedUser);
-        user.addTrip(trip);
+        GivenALoggedUser(loggedUser);
+        GivenAUserFriendOfLoggedUserWithTrips(Arrays.asList(trip));
         when(tripsUserRepository.getTripsOf(user)).thenReturn(Arrays.asList(trip));
         assertThat(service.getTripsByUser(user), is(Arrays.asList(trip)));
+    }
+
+
+    private void GivenALoggedUser(User user) {
+        when(loggedUserService.getLoggedUser()).thenReturn(user);
+    }
+
+    private void GivenANotLoggedUser() {
+        when(loggedUserService.getLoggedUser()).thenThrow(new UserNotLoggedInException());
+    }
+
+    private void GivenAUserFriendOfLoggedUserWithTrips(List<Trip> trips) {
+        user.addFriend(loggedUser);
+        user.addTrip(trip);
+        when(tripsUserRepository.getTripsOf(user)).thenReturn(trips);
     }
 
 }
