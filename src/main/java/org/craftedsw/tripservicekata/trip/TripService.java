3a22ab3de3cffa4733c936b3719f96acc88249b7
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-09-04 10:18:48 +0100

    gitignore and base kata code

---
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -0,0 +1,37 @@
+package org.craftedsw.tripservicekata.trip;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripService {
+
+	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+		List<Trip> tripList = new ArrayList<Trip>();
+		User loggedUser = getLoggedUser();
+		boolean isFriend = false;
+		if (loggedUser != null) {
+			for (User friend : user.getFriends()) {
+				if (friend.equals(loggedUser)) {
+					isFriend = true;
+					break;
+				}
+			}
+			if (isFriend) {
+				tripList = TripDAO.findTripsByUser(user);
+			}
+			return tripList;
+		} else {
+			throw new UserNotLoggedInException();
+		}
+	}
+
+    protected User getLoggedUser() {
+
+        return UserSession.getInstance().getLoggedUser();
+    }
+
+}
