589faede9fb29807ee3711cb57e2c31d89120a1d
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-09-04 11:44:56 +0100

    green: should_return_empty_trip_list_when_loggedUser_is_not_a_friend_of_user

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,6 +4,11 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class TripServiceTest {
 
     @Test(expected = UserNotLoggedInException.class)
@@ -12,11 +17,29 @@ public class TripServiceTest {
         service.getTripsByUser(new User());
     }
 
+    @Test
+    public void should_return_empty_trip_list_when_loggedUser_is_not_a_friend_of_user() {
+        User user = new User();
+        User loggedUser = new User();
+        TripService service = new TripServiceFake(loggedUser);
+        assertThat(service.getTripsByUser(user), is(Arrays.asList()));
+    }
+
     public class TripServiceFake extends TripService {
+
+        private User user;
+
+        public TripServiceFake () {
+            user = null;
+        }
+
+        public TripServiceFake(User user) {
+            this.user = user;
+        }
 
         @Override
         protected User getLoggedUser() {
-            return null;
+            return user;
         }
     }
 	
